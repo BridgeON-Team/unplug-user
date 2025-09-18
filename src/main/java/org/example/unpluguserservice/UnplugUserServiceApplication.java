c420d6ca943b68e3cc365769581243134ba46ccb
minjung <alswjd0240@naver.com>
2025-09-18 19:39:30 +0900

    initial commit

---
+++ src/main/java/org/example/unpluguserservice/UnplugUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.unpluguserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnplugUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnplugUserServiceApplication.class, args);
+	}
+
+}
