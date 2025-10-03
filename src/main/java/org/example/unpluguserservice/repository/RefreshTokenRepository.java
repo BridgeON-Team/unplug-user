aa5d42a40dee86e798435bb3812394e867822b02
minjung <alswjd0240@naver.com>
2025-10-04 00:08:58 +0900

    feat: RefreshToken 생성

---
+++ src/main/java/org/example/unpluguserservice/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package org.example.unpluguserservice.repository;
+
+import org.example.unpluguserservice.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByUsername(String username);
+}
