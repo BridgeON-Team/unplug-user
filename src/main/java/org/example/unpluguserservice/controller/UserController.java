8837ca95ad67a3446f68e87c40e16b07978948ad
minjung <alswjd0240@naver.com>
2025-10-07 19:13:04 +0900

    feat: 유저 정보 조회 및 회원 탈퇴 구현

--- src/main/java/org/example/unpluguserservice/controller/UserController.java
+++ src/main/java/org/example/unpluguserservice/controller/UserController.java
@@ -1,15 +1,10 @@
 package org.example.unpluguserservice.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.unpluguserservice.common.ApiResponse;
-import org.example.unpluguserservice.dto.UserRequestDto;
 import org.example.unpluguserservice.dto.UserResponseDto;
-import org.example.unpluguserservice.entity.User;
-import org.example.unpluguserservice.repository.UserRepository;
 import org.example.unpluguserservice.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -21,17 +16,27 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
-    private final UserRepository userRepository;
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+//    private final UserRepository userRepository;
+//    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    @GetMapping("/info")
+    public ApiResponse<UserResponseDto> userInfo(@RequestHeader(value = "X-Auth-Username", required = false) String username){
+        UserResponseDto response = userService.getUser(username);
+        return new ApiResponse<>(true, "정상적으로 조회했습니다.", response);
+    }
 
+    @DeleteMapping("/withdraw")
+    public ApiResponse<?> deleteUser(@RequestHeader(value = "X-Auth-Username", required = false) String username){
+        userService.deleteUser(username);
+        return new ApiResponse<>(true, "정상적으로 탈퇴되었습니다.");
+    }
 
     // 테스트용 API
-    @GetMapping("/me")
-    public UserInfo me(@RequestHeader(value = "X-Auth-Username", required = false) String username) {
-        if (username == null) throw new IllegalArgumentException("username required");
-        User user = userRepository.findByUsername(username).orElseThrow();
-        return new UserInfo(user.getUserId(), user.getUsername());
-    }
-    public record UserInfo(Long id, String username) {}
+//    @GetMapping("/me")
+//    public UserInfo me(@RequestHeader(value = "X-Auth-Username", required = false) String username) {
+//        if (username == null) throw new IllegalArgumentException("username required");
+//        User user = userRepository.findByUsername(username).orElseThrow();
+//        return new UserInfo(user.getUserId(), user.getUsername());
+//    }
+//    public record UserInfo(Long id, String username) {}
 }
--- src/main/java/org/example/unpluguserservice/service/UserService.java
+++ src/main/java/org/example/unpluguserservice/service/UserService.java
@@ -16,4 +16,15 @@ import java.time.LocalDateTime;
 public class UserService {
     private final UserRepository userRepository;
 
+    public UserResponseDto getUser(String username){
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        return UserResponseDto.fromEntity(user);
+    }
+
+    public void deleteUser(String username){
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+        userRepository.delete(user);
+    }
 }
