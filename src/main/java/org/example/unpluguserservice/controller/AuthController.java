e4e1f8d3af39f94320a3aa083bfd254c65bbda6b
minjung <alswjd0240@naver.com>
2025-10-30 13:45:50 +0900

    fix: 실패 사유 리턴하도록 수정

--- src/main/java/org/example/unpluguserservice/controller/AuthController.java
+++ src/main/java/org/example/unpluguserservice/controller/AuthController.java
@@ -21,35 +21,50 @@ public class AuthController {
     @Operation(summary = "회원가입")
     public ApiResponse<UserResponseDto> registerUser(@Valid @RequestBody UserRequestDto request){
         UserResponseDto response = authService.createUser(request, passwordEncoder);
+        if (response == null){
+            return new ApiResponse<>(false, "회원가입에 실패했습니다.", null);
+        }
         return new ApiResponse<>(true, "회원가입이 정상적으로 완료되었습니다.", response);
     }
 
     @GetMapping("/check/username")
     @Operation(summary = "아이디 중복 확인")
     public ApiResponse<String> checkUsername(@RequestParam("username") String username){
-        authService.checkUsableUsername(username);
-        return new ApiResponse<>(true, "사용 가능한 아이디입니다.", username);
+        if (authService.checkUsableUsername(username)){
+            return new ApiResponse<>(true, "사용 가능한 아이디입니다.", username);
+        }else {
+            return new ApiResponse<>(false, "사용할 수 없는 아이디입니다.", username);
+        }
     }
 
     @GetMapping("/check/nickname")
     @Operation(summary = "닉네임 중복 확인")
     public ApiResponse<String> checkNickname(@RequestParam("nickname") String nickname){
-        authService.checkUsableNickname(nickname);
-        return new ApiResponse<>(true, "사용 가능한 닉네임입니다.", nickname);
+        if (authService.checkUsableNickname(nickname)) {
+            return new ApiResponse<>(true, "사용 가능한 닉네임입니다.", nickname);
+        }else {
+            return new ApiResponse<>(false, "사용할 수 없는 닉네임입니다.", nickname);
+        }
     }
 
     @PostMapping("/login")
     @Operation(summary = "로그인", description = "성공 시 accesstoken 리턴")
     public ApiResponse<LoginResponseDto> login(@RequestBody LoginRequest request) {
         var response = authService.login(request.username(), request.password());
-        return new ApiResponse<>(true, "로그인에 성공했습니다.", new LoginResponseDto(response.accessToken(), response.refreshToken()));
+        if (response.accessToken() == null){
+            return new ApiResponse<>(false, response.data(), new LoginResponseDto(response.data(), null, null));
+        }
+        return new ApiResponse<>(true, "로그인에 성공했습니다.", new LoginResponseDto(response.data(), response.accessToken(), response.refreshToken()));
     }
 
     @PostMapping("/refresh")
     @Operation(summary = "토큰 refresh")
     public ApiResponse<LoginResponseDto> refresh(@RequestBody RefreshRequest request){
         var response = authService.refresh(request.refreshToken());
-        return new ApiResponse<>(true, "success", new LoginResponseDto(response.accessToken(), response.refreshToken()));
+        if (response.accessToken() == null){
+            return new ApiResponse<>(false, response.data(), new LoginResponseDto(response.data(), null, response.refreshToken()));
+        }
+        return new ApiResponse<>(true, "success", new LoginResponseDto(response.data(), response.accessToken(), response.refreshToken()));
     }
 
     @PostMapping("/logout")
@@ -61,5 +76,5 @@ public class AuthController {
 
     public record LoginRequest(String username, String password){}
     public record RefreshRequest(String refreshToken) {}
-    public record LoginResponseDto(String accessToken, String refreshToken){}
+    public record LoginResponseDto(String data, String accessToken, String refreshToken){}
 }
--- src/main/java/org/example/unpluguserservice/service/AuthService.java
+++ src/main/java/org/example/unpluguserservice/service/AuthService.java
@@ -29,6 +29,9 @@ public class AuthService {
 
     @Transactional
     public UserResponseDto createUser(UserRequestDto request, PasswordEncoder passwordEncoder){
+        if(userRepository.findByUsername(request.getUsername()).isPresent()){
+            return null;
+        }
         User user = User.builder()
                 .username(request.getUsername())
                 .name(request.getName())
@@ -41,25 +44,25 @@ public class AuthService {
     }
 
     @Transactional(readOnly = true)
-    public void checkUsableUsername(String username){
-        if(userRepository.findByUsername(username).isPresent()){
-            throw new RuntimeException("이미 존재하는 아이디입니다.");
-        }
+    public boolean checkUsableUsername(String username){
+        return userRepository.findByUsername(username).isEmpty();
     }
 
     @Transactional(readOnly = true)
-    public void checkUsableNickname(String nickname){
-        if (userRepository.findByNickname(nickname).isPresent()){
-            throw new RuntimeException("이미 존재하는 닉네임입니다.");
-        }
+    public boolean checkUsableNickname(String nickname){
+        return userRepository.findByNickname(nickname).isEmpty();
     }
 
     public LoginResponse login(String username, String password){
+        if(userRepository.findByUsername(username).isEmpty()){
+            return new LoginResponse("아이디가 올바르지 않습니다.", null, null);
+        }
+
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 
         if (!passwordEncoder.matches(password, user.getPassword())){
-            throw new IllegalArgumentException("비밀번호가 올바르지 않습니다.");
+            return new LoginResponse("비밀번호가 올바르지 않습니다.", null, null);
         }
 
         String accessToken = jwtUtil.generateAccessToken(user.getUsername());
@@ -68,28 +71,31 @@ public class AuthService {
         RefreshToken r = new RefreshToken(null, refreshToken, user.getUsername(), new Date(System.currentTimeMillis() + refreshExpMs));
         refreshTokenRepository.save(r);
 
-        return new LoginResponse(accessToken, refreshToken);
+        return new LoginResponse("로그인에 성공했습니다.", accessToken, refreshToken);
     }
 
     public LoginResponse refresh(String refreshToken){
+        if (refreshTokenRepository.findByToken(refreshToken).isEmpty()){
+            return new LoginResponse("Invalid Refresh Token ", null, refreshToken);
+        }
         RefreshToken token = refreshTokenRepository.findByToken(refreshToken)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid refresh token"));
 
         if (token.getExpiryDate().before(new Date())){
             refreshTokenRepository.delete(token);
-            throw new IllegalArgumentException("Refresh Token expired");
+            return new LoginResponse("Refresh Token expired", null, refreshToken);
         }
 
         User user = userRepository.findByUsername(token.getUsername())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 
         String newAccess = jwtUtil.generateAccessToken(user.getUsername());
-        return new LoginResponse(newAccess, refreshToken);
+        return new LoginResponse("success", newAccess, refreshToken);
     }
 
     public void logout(String refreshToken){
         refreshTokenRepository.findByToken(refreshToken).ifPresent(refreshTokenRepository::delete);
     }
 
-    public static record LoginResponse(String accessToken, String refreshToken){}
+    public static record LoginResponse(String data, String accessToken, String refreshToken){}
 }
